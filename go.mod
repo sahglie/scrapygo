9776b88ee88bb6740d2082e5412effe34e7c5200
runner <runner@berkeley.edu>
2024-05-22 16:16:38 -0700

    wip

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module scrapygo
 
 go 1.22
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- main.go
+++ main.go
@@ -1,15 +1,35 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"net/http"
+	"os"
+)
 
 func main() {
-	mux := http.NewServeMux()
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
 
+	mux := http.NewServeMux()
 	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("<html>Hello world</html>"))
 	})
 
-	err := http.ListenAndServe("localhost:8080", mux)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	host := os.Getenv("HOST")
+	if host == "" {
+		host = "localhost"
+	}
+
+	addr := fmt.Sprintf("%s:%s", host, port)
+	err = http.ListenAndServe(addr, mux)
 	if err != nil {
 		panic(err)
 	}
